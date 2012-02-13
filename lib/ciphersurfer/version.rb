fd057aa83cac2c7214ec13fc95b554908780ca90
Paolo Perego <thesp0nge@gmail.com>
2012-02-13 14:01:54 +0100

    Fixed typo in bin script

--- lib/ciphersurfer/version.rb
+++ lib/ciphersurfer/version.rb
@@ -2,7 +2,7 @@ module Ciphersurfer
   module Version
     MAJOR = 1
     MINOR = 0
-    PATCH = 3
+    PATCH = 4
     #BUILD = ''
     #STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
