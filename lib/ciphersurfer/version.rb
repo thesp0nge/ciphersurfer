f0dc98b77d9fbe7706579192b1901b726e521631
Paolo Perego <thesp0nge@gmail.com>
2014-10-15 12:04:39 +0200

    version pushed to 1.2

--- lib/ciphersurfer/version.rb
+++ lib/ciphersurfer/version.rb
@@ -1,8 +1,8 @@
 module Ciphersurfer
   module Version
     MAJOR = 1
-    MINOR = 0
-    PATCH = 4
+    MINOR = 2
+    PATCH = 0
     #BUILD = ''
     #STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
